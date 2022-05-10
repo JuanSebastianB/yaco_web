c63de13bcad5eb6fa4069281a2a37f23adbd66ee
Juan Sebastian Becerra <JuanSebastianBB05@gmail.com>
2022-05-09 22:16:26 -0500

    feat: add banner and fit layout to landing page fix: remove useless images

--- src/sections/landing/landing.component.jsx
+++ src/sections/landing/landing.component.jsx
@@ -2,45 +2,58 @@ import React from "react";
 import "./landing.component.css";
 import { Button } from "primereact/button";
 import { Image } from "primereact/image";
-import yacoLogo from "../../../public/assets/images/Isotipo_YACO.svg";
+import yacoLogoSVG from "../../../public/assets/images/Isotipo_YACO.svg";
+import yacoImagotipePNG from "../../../public/assets/images/imagotipo_transparency.png";
 import LogoYaco from "../../commons/logo-yaco.component";
 
 const LandingComponent = () => {
   //TODO -> solve not load images, alternative for the moment appear the alt value on images
   //TODO -> move assets to public folder
   return (
-    <div>
+    <React.Fragment>
       <div className="grid">
         <div className="col">
           <div className="card-container flex align-items-center justify-content-between">
-            <Image src={yacoLogo} alt="Logo YACO" width="150" />
+            <Image src={yacoLogoSVG} alt="Logo YACO" width="150" />
             <div className="card-container size-container-buttons flex align-items-center justify-content-evenly">
               <Button
-                label="Ingresar"
-                className="p-button-raised p-button-text color-green-YACO"
+                label="Registrarse"
+                className="p-button-raised p-button-text color-main-YACO"
               />
               <Button
-                label="Registrarse"
-                className="p-button-raised background-green-YACO"
+                label="Ingresar"
+                className="p-button-raised background-main-YACO"
               />
             </div>
           </div>
         </div>
       </div>
       <div className="grid">
-        <div className="col-6">
-          <p>
+        <div className="col-6 center-vertical-text">
+          <p className={"space-sides-text"}>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s,
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in
           </p>
         </div>
         <div className="col-6">
-          <LogoYaco/>
-          {/*<Image src={yacoLogo} alt="Logo YACO" />*/}
+          <LogoYaco />
         </div>
       </div>
-    </div>
+      <div className="grid">
+        <div className="col background-main-YACO">
+          <h3 className={'text-catch-style cursor-pointer'}>Encuentra lugares increibles!</h3>
+        </div>
+      </div>
+      <div className="grid">
+        <div className="col background-contrast-YACO">
+          <Image src={yacoImagotipePNG} alt="Logo YACO" width="250" />
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
